d002d6d1b386250385ac79e2cf5fd48dc8606294
赵永蒋 <1161528363@qq.com>
2025-05-06 10:15:00 +0800

    视频裁剪取消文件缓存

--- app/src/main/java/cn/xxt/test/TestMainActivity.java
+++ app/src/main/java/cn/xxt/test/TestMainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.aliyun.common.utils.ToastUtil;
 import com.aliyun.player.alivcplayerexpand.constants.GlobalPlayerConfig;
 import com.aliyun.player.aliyunplayerbase.util.OnDownloadClickListener;
 import com.aliyun.vodplayerview.activity.AliyunPlayerSkinActivity;
@@ -28,9 +27,9 @@ public class TestMainActivity extends Activity {
             public void onClick(View v) {
 //                AliyunVideoRecorder.startRecordForResult(TestMainActivity.this, 0, new AliyunSnapVideoParam(), false);
                 GlobalPlayerConfig.mCurrentPlayType = GlobalPlayerConfig.PLAYTYPE.URL;
-                GlobalPlayerConfig.PlayCacheConfig.mEnableCache = false;
-                GlobalPlayerConfig.PlayCacheConfig.mDir = "/storage/emulated/0/Android/data/com.xxt.jxlxandroid/files/ali/cache/";
-                GlobalPlayerConfig.mUrlPath = "https://pic.xxt.cn/n/notice/manage/video/eeccc2fdeb784f19b3e99b71ea4b75ec1.mp4";
+                GlobalPlayerConfig.PlayCacheConfig.mEnableCache = true;
+                GlobalPlayerConfig.PlayCacheConfig.mDir = context.getCacheDir().getAbsolutePath();
+                GlobalPlayerConfig.mUrlPath = "http://pic.xxt.cn/n/circle/dynamic/dynamicfile128187411720435194799.mp4";
                 GlobalPlayerConfig.mTitle = "看看这是啥视频";
                 GlobalPlayerConfig.IS_BARRAGE = false;
                 GlobalPlayerConfig.PlayConfig.circlePlay = false;
@@ -38,7 +37,7 @@ public class TestMainActivity extends Activity {
                 AliyunPlayerSkinActivity.setOnDownloadClickListener(new OnDownloadClickListener() {
                     @Override
                     public void onClickDownload() {
-                        ToastUtil.showToast(context, "点击了下载");
+//                        ToastUtil.showToast(context, "点击了下载");
                     }
                 });
                 AliyunPlayerSkinActivity.start2PlayForResult(TestMainActivity.this, "");
